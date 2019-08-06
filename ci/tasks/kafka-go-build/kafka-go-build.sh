77c5fe3b694ba34837d70b8d9f3516b609c5bf5a
Daniel Lavoie <dlavoie@live.ca>
2019-08-05 21:55:48 -0400

    Initial pull request

--- ci/tasks/kafka-go-build/kafka-go-build.sh
+++ ci/tasks/kafka-go-build/kafka-go-build.sh
@@ -8,7 +8,7 @@ VERSION_FOLDER=version
 ROOT_DIR=`pwd`
 
 cd $SRC_FOLDER/$BUILD_PATH && \
-  ./mvnw -B package && \
-  ./mvnw mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec > ../$VERSION_FOLDER/version && \ 
+  go build 
+  cat version > ../$VERSION_FOLDER/version && \ 
   cd $ROOT_DIR && \
   cp -a $SRC_FOLDER/$BUILD_PATH/* $BUILD_FOLDER
