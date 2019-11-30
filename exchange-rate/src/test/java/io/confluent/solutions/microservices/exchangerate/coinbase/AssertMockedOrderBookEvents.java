dec567a11b89c1a421694aa4136a8a9c97761035
Daniel Lavoie <dlavoie@live.ca>
2019-11-30 16:20:47 -0500

    Fixed exchange rate service unit tests

--- exchange-rate/src/test/java/io/confluent/solutions/microservices/exchangerate/coinbase/AssertMockedOrderBookEvents.java
+++ exchange-rate/src/test/java/io/confluent/solutions/microservices/exchangerate/coinbase/AssertMockedOrderBookEvents.java
@@ -1,30 +1,34 @@
-package io.confluent.solutions.microservices.currencyspotter.coinbase;
+package io.confluent.solutions.microservices.exchangerate.coinbase;
 
 import java.time.Duration;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 
-import io.confluent.solutions.microservices.exchangerate.coinbase.CoinbaseOrderBookEventService;
+import io.confluent.solutions.microservices.exchangerate.ExchangeRate;
+import io.confluent.solutions.microservices.exchangerate.ExchangeRateApplication;
+import io.confluent.solutions.microservices.exchangerate.coinbase.mock.MockedOrderBookEventService;
 import io.confluent.solutions.microservices.exchangerate.coinbase.model.OrderBookEvent;
 import io.confluent.solutions.microservices.exchangerate.coinbase.model.ProductId;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(properties = "coinbase.mock.enabled=false")
-public class AssertCoinbaseOrderBookEvents {
+@SpringBootTest(classes = ExchangeRateApplication.class)
+public class AssertMockedOrderBookEvents {
 	@Autowired
-	private CoinbaseOrderBookEventService coinbaseOrderBookEventService;
+	private MockedOrderBookEventService mockedOrderBookEventService;
+	
+	@MockBean
+	private KafkaTemplate<String, ExchangeRate> exchangeRateTemplate;
 
 	@Test
 	public void fetch10events() {
 
 		// Retreive the order book event stream for the BTC_USD product.
-		Flux<OrderBookEvent> orderBookEventStream = coinbaseOrderBookEventService
+		Flux<OrderBookEvent> orderBookEventStream = mockedOrderBookEventService
 				.getEvents(new ProductId[] { ProductId.BTC_USD })
 
 				// Send a completion signal after 10 notifications.
