9ca7de6511782c3962303c3646061e00bc9325c4
Daniel Lavoie <dlavoie@live.ca>
2019-11-30 15:56:32 -0500

    Fixed Maven Wrapper integration.

--- ci/tasks/maven/maven-package.sh
+++ ci/tasks/maven/maven-package.sh
@@ -8,9 +8,9 @@ VERSION_FOLDER=version
 
 ROOT_DIR=`pwd`
 
-cd $SRC_FOLDER/$BUILD_PATH && \
-  ./mvnw -B package && \
-  mkdir -p ../$VERSION_FOLDER && \
+cd $SRC_FOLDER && \
+  ./mvnw -B -f $BUILD_PATH/pom.xml package && \
+  mkdir -p $VERSION_FOLDER && \
   ./mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec > $ROOT_DIR/$VERSION_FOLDER/version && \
   cd $ROOT_DIR && \
   cp -a $SRC_FOLDER/$BUILD_PATH/* $BUILD_FOLDER
