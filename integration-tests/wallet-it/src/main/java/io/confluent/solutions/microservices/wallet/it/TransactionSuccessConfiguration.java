c4509dcc43673ad05f752877b16828f32c7b2e6a
Daniel Lavoie <dlavoie@live.ca>
2019-12-01 12:26:40 -0500

    Fixed Wallet IT topics configuration

--- integration-tests/wallet-it/src/main/java/io/confluent/solutions/microservices/wallet/it/TransactionSuccessConfiguration.java
+++ integration-tests/wallet-it/src/main/java/io/confluent/solutions/microservices/wallet/it/TransactionSuccessConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("wallet-it.topics.transaction-success")
+@ConfigurationProperties("wallet.topics.transaction-success")
 public class TransactionSuccessConfiguration extends TopicConfiguration {
 
 }
