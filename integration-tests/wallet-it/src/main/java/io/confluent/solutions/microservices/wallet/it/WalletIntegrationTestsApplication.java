4849d86862775887e4373c61d769d44010b7a198
Daniel Lavoie <dlavoie@live.ca>
2019-12-01 11:48:43 -0500

    Removed Kafka Stream from Wallet IT

--- integration-tests/wallet-it/src/main/java/io/confluent/solutions/microservices/wallet/it/WalletIntegrationTestsApplication.java
+++ integration-tests/wallet-it/src/main/java/io/confluent/solutions/microservices/wallet/it/WalletIntegrationTestsApplication.java
@@ -1,9 +1,7 @@
 package io.confluent.solutions.microservices.wallet.it;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
 
-@EnableKafkaStreams
 @SpringBootApplication
 public class WalletIntegrationTestsApplication {
 
