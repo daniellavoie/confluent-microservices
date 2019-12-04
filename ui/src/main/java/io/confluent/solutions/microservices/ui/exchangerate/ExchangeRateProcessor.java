c11401fcdbf883732d182fa21d3adf8c6b44dd9c
Daniel Lavoie <dlavoie@live.ca>
2019-12-04 08:38:49 -0500

    Added Confluent repository to maven central

--- ui/src/main/java/io/confluent/solutions/microservices/ui/exchangerate/ExchangeRateProcessor.java
+++ ui/src/main/java/io/confluent/solutions/microservices/ui/exchangerate/ExchangeRateProcessor.java
@@ -1,10 +1,22 @@
 package io.confluent.solutions.microservices.ui.exchangerate;
 
 import io.confluent.solutions.microservices.ui.model.ExchangeRateDTO;
+import reactor.core.publisher.BufferOverflowStrategy;
+import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
 
-public interface ExchangeRateProcessor {
-	Flux<ExchangeRateDTO> getExchangeRates();
+public class ExchangeRateReactorProcessor implements ExchangeRateProcessor {
+	DirectProcessor<ExchangeRateDTO> processor = DirectProcessor.create();
+	Flux<ExchangeRateDTO> exchangeRateFlux = processor.onBackpressureBuffer().onBackpressureBuffer(1,
+			BufferOverflowStrategy.DROP_OLDEST);
 
-	void updateExchangeRate(String productId, ExchangeRateDTO exchangeRate);
+	@Override
+	public Flux<ExchangeRateDTO> getExchangeRates() {
+		return exchangeRateFlux;
+	}
+
+	@Override
+	public void updateExchangeRate(String productId, ExchangeRateDTO exchangeRate) {
+		processor.onNext(exchangeRate);
+	}
 }
